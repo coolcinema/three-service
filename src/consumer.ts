0b9c90c18faebc27bb5dd1445cf9fb8adfbabe02
Dmitro Lukianov <mr.scrpt@gmail.com>
2026-02-04 13:45:46 +0200

    fix: name

--- src/consumer.ts
+++ src/consumer.ts
@@ -4,16 +4,16 @@ import { GatewayEventsDefinition } from "./_gen/grpc/gateway-service_events_main
 
 export async function startConsumer() {
   const transport = new Events.AmqpTransport(Registry);
-  const consumer = Events.createConsumer(transport, "sales-service");
+  const consumer = Events.createConsumer(transport, "sales-three");
 
-  console.log("[Sales] Starting Consumer...");
+  console.log("[Three] Starting Consumer...");
 
   await consumer.subscribe(
     GatewayEventsDefinition,
     "ticketSold",
     "gateway",
     async (payload) => {
-      console.log(`[Sales] 🎫 EVENT RECEIVED: TicketSold`);
+      console.log(`[Three] 🎫 EVENT RECEIVED: TicketSold`);
       console.log(`   ID: ${payload.ticketId}, Price: ${payload.price}`);
     },
   );
